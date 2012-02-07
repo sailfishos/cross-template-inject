16e5bdae9949953227d8e41b7740a41cc0429142
Carsten Munk <carsten.munk@gmail.com>
2012-02-07 12:00:35 +0100

    Initial commit of to x86 injections
    Signed-off-by: Carsten Munk <carsten.munk@gmail.com>

---
+++ precheckin.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+for x in glibc glibc-devel glibc-headers kernel-headers; do
+	sed "s/ExclusiveArch: none//g" cross-template-inject.spec > cross-$x-inject.spec
+	sed -i "s/@PACKAGENAME@/$x/g" cross-$x-inject.spec
+done
